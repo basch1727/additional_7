0c5c0359b2a3f691437af7d548ffced2c4e94e12
basch1727 <basch19900@gmail.com>
2018-10-03 23:39:42 +0300

    sol

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  function suggest(r,c) {
+  const arr = [];
+  const blockRow = Math.floor(r / 3) * 3;
+  const blockCol = Math.floor(c / 3) * 3;
+    for (let i = 0; i < 9; i++) {
+      arr.push([matrix[r][i], matrix[i][c], matrix[blockRow + i % 3][blockCol + Math.floor(i / 3)]])
+    }
+    return arr;
+  }     
+    for (let r = 0; r < 9; r++) {
+      for (let c = 0; c < 9; c++) {
+        if (matrix[r][c] === 0) {
+          let n = suggest(r, c);
+            for (let i=0;i<9;i++) {
+              matrix[r][c] = n[i];
+                  
+        
+            }
+         }
+      }
+    }
+return matrix;
 }
